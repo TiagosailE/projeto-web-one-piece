4f9f0616a7521070dbb6a42c15314666d6a7cc4d
TiagosailE <126528408+TiagosailE@users.noreply.github.com>
2023-06-09 21:31:04 -0300

    Primeira versão do site One Piece.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('#videos .container').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});$('#fotos').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+		}
+	});
+});
